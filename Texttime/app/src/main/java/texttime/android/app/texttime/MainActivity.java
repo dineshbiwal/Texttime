2f96f8d0b15a0325e92122e85883016a367b51c6
Aashish Bhatnagar <aashishb@texttime.com>
2017-02-06 13:08:45 +0530

    Aashish second commit

--- Texttime/app/src/main/java/texttime/android/app/texttime/MainActivity.java
+++ Texttime/app/src/main/java/texttime/android/app/texttime/MainActivity.java
@@ -2,6 +2,7 @@ package texttime.android.app.texttime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Dinesh_Text on 2/6/2017.
@@ -12,5 +13,7 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        TextView tv=new TextView(this);
+        tv.setText("git text");
     }
 }
